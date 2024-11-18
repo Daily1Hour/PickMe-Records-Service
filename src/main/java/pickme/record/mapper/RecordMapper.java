dd9153a1143bc34e8940a608d67c8d89b9fb6629
yeongjin <mygxxgleacc@gmail.com>
2024-11-18 16:28:32 +0900

    refact: updateEntity -> updateEntityFromDto

--- src/main/java/pickme/record/mapper/RecordMapper.java
+++ src/main/java/pickme/record/mapper/RecordMapper.java
@@ -14,6 +14,6 @@ public interface RecordMapper {
 
     RecordResponseDto toResponseDto(Record record);
 
-    void updateEntity(RecordUpdateDto dto, @MappingTarget Record record);
+    void updateEntityFromDto(RecordUpdateDto dto, @MappingTarget Record record);
 
 }
