568c37af500aace932edc46f982fbdb8464f238a
yxxngjxn <yxxngjxn99@gmail.com>
2024-11-20 15:49:58 +0900

    refact: API mapping url record -> records 수정

--- src/main/java/pickme/record/controller/RecordController.java
+++ src/main/java/pickme/record/controller/RecordController.java
@@ -20,7 +20,7 @@ import java.util.Optional;
 import java.util.stream.Collectors;
 
 @RestController
-@RequestMapping("/api/record")
+@RequestMapping("/records")
 @Tag(name = "Record", description = "면접 기록 관리 API")
 public class RecordController {
 
