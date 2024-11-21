ba50594552c9d1a935cc2f0aedd56714dc72486d
yxxngjxn <yxxngjxn99@gmail.com>
2024-11-21 17:21:47 +0900

    refactor: RecordResponseDTO 필드 추가 및 수정

--- src/main/java/pickme/record/dto/RecordResponseDTO.java
+++ src/main/java/pickme/record/dto/RecordResponseDTO.java
@@ -16,11 +16,17 @@ public class RecordResponseDTO {
     @Schema(description = "User ID", example = "user123")
     private String userId;
 
+    @Schema(description = "Enterprise Name", example = "Day Company")
+    private String enterpriseName;
+
     @Schema(description = "Category", example = "1st interview")
     private String category;
 
-    @Schema(description = "Content", example = "(1분 자기소개) 안녕하십니까. 저는...")
-    private String content;
+    @Schema(description = "Question", example = "1분 자기소개")
+    private String question;
+
+    @Schema(description = "Answer", example = "안녕하십니까. 뫄뫄 기업 솨솨 직무에 지원한 김땡땡입니다.")
+    private String answer;
 
     @Schema(description = "Creation Time", example = "2024-01-01T12:34:56Z")
     private Date createdAt;
