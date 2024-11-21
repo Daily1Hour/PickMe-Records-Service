c4a75a06b3c63161a9558bbd8a3e3c1a386ea074
yxxngjxn <yxxngjxn99@gmail.com>
2024-11-21 17:25:16 +0900

    refactor: RecordUpdateDTO 필드 추가 및 수정

--- src/main/java/pickme/record/dto/RecordUpdateDTO.java
+++ src/main/java/pickme/record/dto/RecordUpdateDTO.java
@@ -8,12 +8,20 @@ import lombok.Data;
 @Schema(description = "DTO for updating an existing interview record")
 public class RecordUpdateDTO {
 
+    @Schema(description = "Enterprise Name", example = "Day Company")
+    @NotBlank(message = "Enterprise Name is required")
+    private String enterpriseName;
+
     @Schema(description = "Category", example = "1st interview")
     @NotBlank(message = "Category is required")
     private String category;
 
-    @Schema(description = "Content", example = "(1분 자기소개) 안녕하십니까. 저는...")
-    @NotBlank(message = "Content is required")
-    private String content;
+    @Schema(description = "Question", example = "1분 자기소개")
+    @NotBlank(message = "Question is required")
+    private String question;
+
+    @Schema(description = "Answer", example = "안녕하십니까. 뫄뫄 기업 솨솨 직무에 지원한 김땡땡입니다.")
+    @NotBlank(message = "Answer is required")
+    private String answer;
 
 }
