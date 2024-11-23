c3c3ab729f786747c19355bbb6db0c71b497efbf
yxxngjxn <yxxngjxn99@gmail.com>
2024-11-23 14:56:51 +0900

    refactor: RecordRepository 사용하지 않는 import 삭제

--- src/main/java/pickme/record/repository/RecordRepository.java
+++ src/main/java/pickme/record/repository/RecordRepository.java
@@ -1,6 +1,5 @@
 package pickme.record.repository;
 
-import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 import pickme.record.model.Record;
